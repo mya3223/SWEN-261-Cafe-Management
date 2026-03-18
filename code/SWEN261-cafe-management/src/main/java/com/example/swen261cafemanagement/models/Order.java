237cc8cb9989bb126f26ba0dced8c3019c191bee
mya3223 <mya3223@g.rit.edu>
2026-03-18 21:50:57 +0400

    Add files via upload

--- code/SWEN261-cafe-management/src/main/java/com/example/swen261cafemanagement/models/Order.java
+++ code/SWEN261-cafe-management/src/main/java/com/example/swen261cafemanagement/models/Order.java
@@ -1,41 +1,59 @@
-package com.example.swen261cafemanagement.models;
+package com.example.assignment1.models;
 
 public class Order {
-    private String ordermessage;
     private String orderId;
-    private String orderStatus;
+    private String orderMessage;
+    private String items;
+    private String status;
+    private double totalPrice;
 
-    public Order (){}
-    public Order( String orderId, String orderStatus,String ordermessage) {
-        this.ordermessage = ordermessage;
+    public Order() {}
+
+    public Order(String orderId, String orderStatus, String orderMessage, String items, String status, double totalPrice) {
         this.orderId = orderId;
-        this.orderStatus = orderStatus;
+        this.orderMessage = orderMessage;
+        this.items = items;
+        this.status = status;
+        this.totalPrice = totalPrice;
     }
 
-    public String getOrdermessage() {
-        return ordermessage;
+    public String getItems(){
+        return items;
     }
 
-    public void setOrdermessage(String ordermessage) {
-        this.ordermessage = ordermessage;
+    public double getTotalPrice() {
+        return totalPrice;
     }
 
-    public String getorderId() {
+    public String getStatus() {
+        return status;
+    }
+
+    public String getOrderId() {
         return orderId;
     }
 
+
+    public String getOrderMessage() {
+        return orderMessage;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setItems(String items) {
+        this.items = items;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
     public void setOrderId(String orderId) {
         this.orderId = orderId;
     }
 
-    public String getorderStatus() {
-        return orderStatus;
-    }
-
-    public void setorderStatus(String orderStatus) {
-        this.orderStatus = orderStatus;
-    }
     public boolean isCompleted() {
-        return "Delivered".equalsIgnoreCase(orderStatus);
+        return "Delivered".equalsIgnoreCase(status);
     }
 }
