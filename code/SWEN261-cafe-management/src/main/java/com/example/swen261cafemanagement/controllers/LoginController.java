99a7c91391f3a8b7fdf7e522879978842d732d93
FrancoisMteRIT <flm2323@rit.edu>
2026-03-22 10:57:50 +0100

    Added error messages

--- code/SWEN261-cafe-management/src/main/java/com/example/swen261cafemanagement/controllers/LoginController.java
+++ code/SWEN261-cafe-management/src/main/java/com/example/swen261cafemanagement/controllers/LoginController.java
@@ -4,6 +4,7 @@ package com.example.swen261cafemanagement.controllers;
 import com.example.swen261cafemanagement.models.User;
 import com.example.swen261cafemanagement.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,20 +12,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
     private UserService userService;
+
     public LoginController(UserService userService) {
         this.userService = userService;
     }
+
     @RequestMapping("/login")
     public String getLogin() {
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String email, @RequestParam String password) {
+    public String login(@RequestParam String email, @RequestParam String password, Model model) {
 
         User user = userService.findByUserByEmail(email);
 
         if (user == null) {
+            model.addAttribute("errorMsg", "Email or password incorrect");
             return "login";
         }
 
@@ -32,6 +36,7 @@ public class LoginController {
         if (password.equals(user.getPassword())) {
             return "dashboard";
         } else {
+            model.addAttribute("errorMsg", "Nom d’utilisateur ou mot de passe incorrect");
             return "login";
         }
     }
