e8cb110ca2f77cd4ef6474d71a012907bc9fc2ae
Mouza Alameri <100403792+MouzaAlameri-sec@users.noreply.github.com>
2026-04-19 21:30:49 +0400

    Add files via upload

---
+++ code/SWEN261-cafe-management/src/main/java/com/example/swen261cafemanagement/controllers/ItemController.java
@@ -0,0 +1,42 @@
+package com.example.swen261cafemanagement.controllers;
+
+import com.example.swen261cafemanagement.models.Item;
+import com.example.swen261cafemanagement.service.ItemService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemService service;
+
+    public ItemController(ItemService service) {
+        this.service = service;
+    }
+
+    // VIEW PAGE
+    @GetMapping
+    public String viewItems(Model model) {
+        model.addAttribute("items", service.getAllItems());
+        return "items";
+    }
+
+    // ADD ITEM
+    @PostMapping("/add")
+    public String addItem(@RequestParam String name,
+                          @RequestParam double price,
+                          @RequestParam int quantity) {
+
+        service.addItem(new Item(null, name, price, quantity));
+        return "redirect:/items";
+    }
+
+    // DELETE
+    @GetMapping("/delete/{id}")
+    public String deleteItem(@PathVariable Long id) {
+        service.deleteItem(id);
+        return "redirect:/items";
+    }
+}
