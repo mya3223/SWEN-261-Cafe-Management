01fbd8d80a621378a45d7d1aa9080d39503faaf2
AliHamie5 <145132758+AliHamie5@users.noreply.github.com>
2026-04-19 23:56:08 +0400

    Update OrderController.java

--- code/SWEN261-cafe-management/src/main/java/com/example/swen261cafemanagement/controllers/OrderController.java
+++ code/SWEN261-cafe-management/src/main/java/com/example/swen261cafemanagement/controllers/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
